07e3094ea59141efc715944a453a7fced97719b3
nayrk <1281077+nayrk@users.noreply.github.com>
2023-05-26 11:42:33 -0700

    Update app.py with mount point

--- app.py
+++ app.py
@@ -34,5 +34,11 @@ def read_file():
     contents = f.read()
     return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
 
+@app.route('/read_mounted_file')
+def read_file():
+    f = open("/data/testfile.txt")
+    contents = f.read()
+    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080")
