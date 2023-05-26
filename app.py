5ec1065150cf6715e7f1cf2e809c00deb411b84f
nayrk <1281077+nayrk@users.noreply.github.com>
2023-05-26 15:37:51 -0700

    Update app.py redis-2

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def is_redis_available(g):
 def get_redis():
     if not hasattr(g, 'redis'):
         print("Connecting to redis db")
-        g.redis = Redis(host="redis", db=0, password=redis_password, socket_timeout=5, decode_responses=True)
+        g.redis = Redis(host="redis-2", db=0, password=redis_password, socket_timeout=5, decode_responses=True)
 
     is_redis_available(g.redis)
     return g.redis
